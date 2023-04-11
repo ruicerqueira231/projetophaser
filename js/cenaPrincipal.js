d453daed4f33ef7f05f94c86dec084b9edf255e3
tiagooliveiragithub <contacto.tiagooliveira@gmail.com>
2023-04-11 02:36:36 +0100

    final mesmo?

--- js/cenaPrincipal.js
+++ js/cenaPrincipal.js
@@ -47,7 +47,7 @@ export default class CenaPrincipal extends Phaser.Scene {
         map.createStaticLayer("Camada de Blocos 3", tileset,0,0);
         const camada5 = map.createStaticLayer("Camada de Blocos 5", tileset,0,0);
         this.player = new Jogador({scene:this, x:100, y:100, texture:'menina', frame: 'townsfolk_f_walk_1'});
-        this.inimigo = new Inimigo({scene:this, x:300, y:300, texture:'inimigos', frame: 'golem_idle_1', scale: 2, vida: 300, tipo: "goglem"});
+        this.inimigo = new Inimigo({scene:this, x:300, y:300, texture:'inimigos', frame: 'golem_idle_1', scale: 2, vida: 200, tipo: "goglem"});
           
         const camada4 = map.createStaticLayer("Camada de Blocos 4", tileset,0,0);
 
